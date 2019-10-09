bf4236e2430ffd2165a50e525f95f60804ba1196
Thai Nguyen <thaiomega3@gmail.com>
2019-10-09 15:29:54 -0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Thai Nguyen on 10/9/19.
+//  Copyright © 2019 Thai Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
